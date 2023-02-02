050745f4930b6e42ca431d4750c6b22779261fc5
ayedun <88404572+ayedun@users.noreply.github.com>
2023-02-02 10:11:38 -0800

    Finish all tasks

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,6 @@
 module Enumerable
   # Your code goes here
+  
 end
 
 # You will first have to define my_each
@@ -8,4 +9,99 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return self.to_enum unless block_given?
+
+    for item in self
+      yield item
+    end
+  end
+
+
+  def my_each_with_index
+    return self.to_enum unless block_given?
+    for i in 0..self.length-1
+      yield self[i], i
+    end
+    self
+  end
+
+  def my_select
+    result = []
+    return self.to_enum unless block_given?
+
+    for element in self
+      if yield(element)
+        result<< element
+      end
+    end
+    result
+  end
+
+  def my_all?
+    state = true
+    for el in self
+      if !(yield(el))
+        state = false
+      end
+    end 
+    return state
+  end
+
+  def my_any?
+    state = false
+    for el in self
+      if yield(el)
+        state=  true
+      end
+    end
+    state
+  end
+  
+  def my_none?
+    state = true
+    for el in self
+      if yield(el)
+        state = false
+        break
+      end
+    end
+    state
+  end
+
+  def my_count
+    count = 0
+    return self.length unless block_given?
+    for el in self
+      if yield(el)
+        count += 1
+      end
+    end
+    count
+  end
+  def my_map
+    result = []
+    for el in self 
+      result<<yield(el)
+    end
+    result
+  end
+
+  def my_inject(initial_value)
+    total = initial_value
+
+    for el in self
+      total = yield total, el
+    end
+    total
+    
+  end
+  
+
+
+
+
+
+
+
 end
